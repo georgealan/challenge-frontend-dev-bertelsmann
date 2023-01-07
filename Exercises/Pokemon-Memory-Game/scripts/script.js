d458238b642978ed9e44bdac48d9dbc23a2a07af
George Alan <georgealanrufo@gmail.com>
2023-01-06 21:06:35 -0300

    All html tags cloned, now each div have a clone

--- Exercises/Pokemon-Memory-Game/scripts/script.js
+++ Exercises/Pokemon-Memory-Game/scripts/script.js
@@ -1,4 +1,4 @@
-for(var i = 1; i <= 10; i++) {
+for(var i = 1; i <= 18; i++) {
     const card = document.createElement('div');
     const imgFront = document.createElement('img');
     const imgBack = document.createElement('img');
@@ -7,15 +7,21 @@ for(var i = 1; i <= 10; i++) {
     card.setAttribute('id', 'card-'+i);
     card.setAttribute('class', 'card');
 
+    // imgFront.setAttribute('id', 'front-face-'+i);
     imgFront.setAttribute('class', 'front-face');
     imgFront.setAttribute('src', 'assets/images/pokemons/pokemon-' + randonNumber + '.png');
-    imgFront.setAttribute('alt', 'card-img');
+    imgFront.setAttribute('alt', 'Image card front view');
     
+    // imgBack.setAttribute('id', 'back-face-'+i);
     imgBack.setAttribute('class', 'back-face');
     imgBack.setAttribute('src', 'assets/images/pokeball.png');
-    imgBack.setAttribute('alt', 'card-back-img');
+    imgBack.setAttribute('alt', 'Image card back view');
 
     document.getElementById('board-game').append(card);
     document.getElementById('card-'+i).append(imgFront);
     document.getElementById('card-'+i).append(imgBack);
+
+    const node = document.getElementById('card-'+i);
+    const clone = node.cloneNode(card);
+    document.getElementById('board-game').appendChild(clone);
 }
