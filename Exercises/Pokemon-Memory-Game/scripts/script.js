7cc3909aebb386df212f76981be8715617a33860
George Alan <georgealanrufo@gmail.com>
2023-01-08 20:23:29 -0300

    add functions flip card, match cards

--- Exercises/Pokemon-Memory-Game/scripts/script.js
+++ Exercises/Pokemon-Memory-Game/scripts/script.js
@@ -18,11 +18,99 @@ for(var i = 1; i <= 15; i++) {
     imgBack.setAttribute('src', 'assets/images/pokeball.png');
     imgBack.setAttribute('alt', 'Image card back view');
 
+    // Insert html elements in the DOM
     document.getElementById('board-game').append(card);
     document.getElementById('card-'+i).append(imgFront);
     document.getElementById('card-'+i).append(imgBack);
 
+    // Cloning the div element for duplicate the deck, with that will have 2 equal cards.
     const node = document.getElementById('card-'+i);
     const clone = node.cloneNode(card);
     document.getElementById('board-game').appendChild(clone);
 }
+
+// Selecting all divs with class name .card
+const cards = document.querySelectorAll('.card');
+
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let disableDecks = false;
+let matchedCard = 0;
+
+function flipCard() {
+    if (disableDecks) return; // Lock the board for nothing alow many clicks.
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        // First Click
+        hasFlippedCard = true;
+        firstCard = this;
+        
+        return;
+    }
+
+    // Second click
+    secondCard = this;
+
+    matchCards(firstCard.id, secondCard.id);
+}
+
+function matchCards(firstCardId, secondCardId) {
+    if(firstCardId === secondCardId) {
+        matchedCard++;
+        console.log(matchedCard);
+
+        if(matchedCard == 15) {
+            setTimeout(() => {
+                return shuffleCards();
+            }, 1000);
+        }
+
+        disableCards();
+
+    } else {
+        unflipCards();
+    }
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    resetBoard();
+}
+
+function unflipCards() {
+    disableDecks = true;
+
+    setTimeout(() => {
+        firstCard.classList.add('shake'); 
+        secondCard.classList.add('shake');
+    }, 400);
+
+    setTimeout(() => {
+        firstCard.classList.remove('shake', 'flip');
+        secondCard.classList.remove('shake', 'flip');
+        resetBoard();
+    }, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, disableDecks] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+// Adjust logic!
+function shuffleCards() {
+    console.log('in shuffle function');
+    matchedCard = 0;
+    cards.forEach(card => {
+        card.classList.remove('flip');
+        card.addEventListener('click', flipCard);
+    });
+}
+
+// Event listener that know when one of all divs receive an click
+cards.forEach(card => card.addEventListener('click', flipCard));
